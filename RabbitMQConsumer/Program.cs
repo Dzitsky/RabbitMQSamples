cff0b10e64a4922f206be3a00f00d47589534c84
Automerge <automerge@rts-tender.ru>
2023-03-09 22:26:27 +0300

    Add Masstransit

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQConsumer
+{
+    internal class Program
+    {
+		static void Main()
+		{
+			using (var connection = GetRabbitConnection())
+			using (var channel = connection.CreateModel())
+			{
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += (model, ea) =>
+				{
+					var body = ea.Body;
+					string message = Encoding.UTF8.GetString(body.ToArray());
+
+					//throw new Exception("Всё пропало!");
+
+					Console.WriteLine(" [x] Received {0}", message);
+				};
+
+				
+				channel.BasicConsume(queue: "Say.Hello",
+									 autoAck: true,
+									 consumer: consumer);
+
+				Console.WriteLine(" Press [enter] to exit.");
+				Console.ReadLine();
+			}
+		}
+
+		static private IConnection GetRabbitConnection()
+		{
+			ConnectionFactory factory = new ConnectionFactory
+			{
+                UserName = "guest",
+                Password = "guest",
+                //VirtualHost = "hyvbmeov",
+                HostName = "woodpecker.rmq.cloudamqp.com"
+            };
+			IConnection conn = factory.CreateConnection();
+			return conn;
+		}
+
+	}
+}
---
+++ RabbitMQProduser/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQProduser
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			using (var connection = GetRabbitConnection())
+			using (var channel = connection.CreateModel())
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					string message = DateTime.UtcNow.ToString();
+
+					var body = Encoding.UTF8.GetBytes(message);
+
+					channel.BasicPublish(exchange: "",
+										 routingKey: "Say.Hello",
+										 basicProperties: null,
+										 body: body);
+					
+					Console.WriteLine($"Отправлено {message}");
+				}
+			}
+
+			Console.WriteLine(" Press [enter] to exit.");
+			Console.ReadLine();
+		}
+
+		static private IConnection GetRabbitConnection()
+		{
+			ConnectionFactory factory = new ConnectionFactory
+			{
+				UserName = "guest",
+				Password = "guest",
+				VirtualHost = "VirtualHost",
+				HostName = ""
+            };
+			IConnection conn = factory.CreateConnection();
+			return conn;
+		}
+	}
+}
